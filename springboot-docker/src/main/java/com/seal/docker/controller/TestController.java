940385df9746e7911db34b6f1c5974b64ca9fa5b
youngMen1 <fengzhiqiangzxx199644>
2019-08-12 17:18:18 +0800

    desc:添加springboot-docker

---
+++ springboot-docker/src/main/java/com/seal/docker/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.seal.docker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhiqiang.feng
+ * @version 1.0
+ * @date-time 2019/8/12 17:04
+ * @description
+ **/
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+}
